d0eef25348450ee25b88f65d92a948f147843587
stephenwayar <stephenbuluswayar@gmail.com>
2022-08-01 12:46:31 +0100

    more integration tests

--- controllers/NoteController.js
+++ controllers/NoteController.js
@@ -1,32 +1,51 @@
 const Note = require('../models/Note')
 
 exports.get_notes = async (req, res) => {
-  const notes = await Note.find({})
-  res.status(200).json(notes)
+  try{
+    const notes = await Note.find({})
+    res.status(200).json(notes)
+  }catch(error){
+    res.status(500).end()
+  }
 }
 
 exports.get_note = async (req, res, next) => {
-  const note = await Note.findById(req.params.id)
-  if (note){
-    res.json(note)
-  }else {
-    res.status(404).end()
+  try{
+    const note = await Note.findById(req.params.id)
+    if (note){
+      res.json(note)
+    }else {
+      res.status(404).end()
+    }
+  }catch(error){
+    next(error)
   }
 }
 
 exports.delete_note = async (req, res, next) => {
-  await Note.findByIdAndDelete(req.params.id)
-  res.status(200).end()
+  try{
+    await Note.findByIdAndDelete(req.params.id)
+    res.status(200).end()
+  }catch(error){
+    res.status(400).json({
+      suceess: false,
+      message: "Note has already been deleted from server"
+    })
+  }
 }
 
 exports.update_note = async (request, response, next) => {
   const { content, important } = request.body
-  const updatedNote = await Note.findByIdAndUpdate(
-    request.params.id,
-    { content, important },
-    { new: true, runValidators: true, context: 'query' }
-  )
-  response.json(updatedNote)
+  try{
+    const updatedNote = await Note.findByIdAndUpdate(
+      request.params.id,
+      { content, important },
+      { new: true, runValidators: true, context: 'query' }
+    )
+    response.json(updatedNote)
+  }catch(error){
+    next(error)
+  }
 }
 
 exports.create_note = async (req, res, next) => {
@@ -36,6 +55,10 @@ exports.create_note = async (req, res, next) => {
     important: body.important || false,
     date: new Date(),
   })
-  const savedNote = await note.save()
-  res.status(201).json(savedNote)
+  try{
+    const savedNote = await note.save()
+    res.status(201).json(savedNote)
+  }catch(error){
+    next(error)
+  }
 }
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
-const api = supertest(app)
+const api = require('../helpers/api')
 const Note = require('../models/note')
 const helper = require('../helpers/testing')
 
@@ -73,6 +71,40 @@ test('note without content is not added', async () => {
 
   expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
 }, 20000)
+
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb()
+
+  const noteToView = notesAtStart[0]
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const processedNoteToView = JSON.parse(JSON.stringify(noteToView))
+
+  expect(resultNote.body).toEqual(processedNoteToView)
+})
+
+test('a note can be deleted', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToDelete = notesAtStart[0]
+
+  await api
+    .delete(`/api/notes/${noteToDelete.id}`)
+    .expect(204)
+
+  const notesAtEnd = await helper.notesInDb()
+
+  expect(notesAtEnd).toHaveLength(
+    helper.initialNotes.length - 1
+  )
+
+  const contents = notesAtEnd.map(r => r.content)
+
+  expect(contents).not.toContain(noteToDelete.content)
+})
 
 afterAll(() => {
   mongoose.connection.close()
